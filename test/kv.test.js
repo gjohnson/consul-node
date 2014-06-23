deafd460469d130fbd081dc44ef870c41bba75d3
chris <chris@clanofthecloud.com>
2014-06-23 22:25:23 +0200

    added KV.watch

--- test/kv.test.js
+++ test/kv.test.js
@@ -2,7 +2,7 @@
 var Consul = require('..');
 var assert = require('assert');
 
-describe('consul.kv', function () {
+describe.only('consul.kv', function () {
 
   describe('#get', function () {
     describe('when not in "strict mode"', function () {
@@ -51,4 +51,76 @@ describe('consul.kv', function () {
     });
   });
 
+  describe('#watch', function() {
+    var consul = new Consul();
+    it('calls back immediately with value', function (done) {
+      var stop = consul.kv.watch('hello', function (err, values) {
+        if (err) return done(err);
+        assert(Array.isArray(values));
+        assert(values.length === 1);
+        values.forEach(function (value) {
+          assert(value.key === 'hello');
+          assert(value.value === 'world');
+        });
+        stop();
+        done();
+      });
+    });
+
+    it('calls back again if value is changed', function (done) {
+      this.timeout(12000);
+      var count = 0;
+      var stop = consul.kv.watch('hello', function (err, values) {
+        count++;
+        if (err) return done(err);
+        switch (count) {
+          case 1:
+            assert(values[0].key === 'hello');
+            assert(values[0].value === 'world');
+            consul.kv.put('hello','all', function (err) { // change value, handler will be called again
+              if (err) return done(err);
+            });
+            break;
+          case 2:
+            assert(values[0].key === 'hello');
+            assert(values[0].value === 'all');
+            stop();
+            done();
+        }
+      });
+    });
+
+    it('retries http calls after 10s', function (done) {
+      this.timeout(12000);
+      var count = 0;
+      var stop = consul.kv.watch('hello', function (err, values) {
+        count++;
+        if (err) return done(err);
+        switch (count) {
+          case 1:
+            assert(values[0].key === 'hello');
+            assert(values[0].value === 'all');
+            consul.kv.put('hello','world', function (err) { // change value, handler will be called again
+              if (err) return done(err);
+            });
+            break;
+          case 2:
+            assert(values[0].key === 'hello');
+            assert(values[0].value === 'world');
+            setTimeout(function() {
+              consul.kv.put('hello','again', function (err) { // change value back, handler will be called again
+                if (err) return done(err);
+              });
+            }, 10500);
+            break;
+          case 3:
+            assert(values[0].key === 'hello');
+            assert(values[0].value === 'again');
+            stop();
+            done();
+        }
+      });
+    });
+
+  });
 });
